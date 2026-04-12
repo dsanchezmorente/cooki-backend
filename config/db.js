ed00f9fb051dc316f710816810bcdf5ee70308f2
dsanchezmorente <dsanchezmorente@gmail.com>
2026-04-12 18:24:56 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
   const connection = mysql.createConnection({
-  host: process.env.MYSQL_PUBLIC_URL || 'localhost',
-  user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || 'temporal',
-  database: process.env.MYSQL_DATABASE || 'cooki'
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'temporal',
+  database: process.env.DB_NAME || 'cooki'
 });
 
 console.log('Conectando a la base de datos...'+connection.config.host);
