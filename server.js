8192ddfb3136665e79c1782600cba28ded880731
dsanchezmorente <dsanchezmorente@gmail.com>
2026-04-12 20:53:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`[REQUEST] ${req.method} ${req.originalUrl}`);
+  next();
+});
+
 app.use('/usuarios', require('./routes/usuarios'));
 app.use('/recetas', require('./routes/recetas'));
+
+app.use((err, req, res, next) => {
+  console.error('[UNHANDLED ERROR]', err);
+  res.status(500).json({ message: 'Internal server error' });
+});
+
+process.on('uncaughtException', err => {
+  console.error('[UNCAUGHT EXCEPTION]', err);
+});
+
+process.on('unhandledRejection', reason => {
+  console.error('[UNHANDLED REJECTION]', reason);
+});
 
 app.listen(PORT, () => {
   console.log(`COOKI server running on port ${PORT}`);
